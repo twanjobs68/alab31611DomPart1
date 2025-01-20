63fcd37d75576c56c5190bd70ac04605e4efa1d3
Antwan Grant <twanjobs68@gmail.com>
2025-01-20 17:05:56 -0500

    added html, js, and css to vs code for lab testing. First commit

---
+++ script/alab31611DomAManipPrt1.js
@@ -0,0 +1,6 @@
+// javascript for Alab316.1.1 Dom Manipulation Part 1 Lab
+const mainEL = document.querySelector("main");
+
+console.log(
+  `This is now a representative of the "main" element in the html: ${mainEL}`
+);
